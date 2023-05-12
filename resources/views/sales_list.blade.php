8563e0e9bc4f0176825ef7973eeae6381e5d6f8e
Rahmat Raditya <rahmatraditya@Rahmats-MacBook-Pro.local>
2023-05-12 08:43:49 +0700

    Apply the sidebar

--- resources/views/sales_list.blade.php
+++ resources/views/sales_list.blade.php
@@ -6,13 +6,78 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.0/css/all.css" integrity="sha384-lZN37f5QGtY3VHgisS14W3ExzMWZxybE1SJSEsQp9S+oqd12jhcu+A56Ebc1zFSJ" crossorigin="anonymous">
         <title>List Sales</title>
+        <style>
+          li {
+            list-style: none;
+            margin: 20px 0 20px 0;
+          }
+    
+          a {
+            text-decoration: none;
+          }
+    
+          .sidebar {
+            width: 250px;
+            height: 100vh;
+            position: fixed;
+            margin-left: -300px;
+            transition: 0.4s;
+          }
+    
+          .active-main-content {
+            margin-left: 250px;
+          }
+    
+          .active-sidebar {
+            margin-left: 0;
+          }
+    
+          #main-content {
+            transition: 0.4s;
+          }
+        </style>
     </head>
 
     <body>
-        <div class="container">
+        <div class="sidebar p-4 bg-primary" id="sidebar">
+            <h4 class="mb-5 text-white">SalesVisit</h4>
+            <li>
+              <a class="text-white" href="#">
+              <i class="fas fa-tags pr-2"></i>
+                Transaksi
+              </a>
+            </li>
+            <li>
+              <a class="text-white" href="<?php echo url('sales') ?>">
+                <i class="fas fa-users pr-2"></i>
+                Master Sales
+              </a>
+            </li>
+            <li>
+              <a class="text-white" href="<?php echo url('barangs') ?>">
+                <i class="fas fa-shopping-bag pr-2"></i>
+                Master Barang
+              </a>
+            </li>
+            <li>
+              <a class="text-white" href="<?php echo url('outlets') ?>">
+                <i class="fas fa-store-alt pr-2"></i>
+                Master Outlet
+              </a>
+            </li>
+          </div>
+        </div>
+
+
+        <div class="container" id="main-content">
             <div class="row justify-content-center align-items-center">
-                <div class="col-md-8">
+                <div class="col-md-12 pt-4">
+                    <button class="btn btn-primary" id="button-toggle">
+                        <i class="fas fa-bars"></i>
+                    </button>
+
                     <div class="d-flex justify-content-between mt-5 mb-2">
                         <h3>List Sales</h3>
                         <a href="{{ route('sales.create') }}"><button class="btn btn-primary alig">Tambah Sales</button></a>
@@ -53,5 +118,17 @@
                 </div>
             </div>
         </div>
+
+        <script>
+            // event will be executed when the toggle-button is clicked
+            document.getElementById("button-toggle").addEventListener("click", () => {
+
+            // when the button-toggle is clicked, it will add/remove the active-sidebar class
+            document.getElementById("sidebar").classList.toggle("active-sidebar");
+
+            // when the button-toggle is clicked, it will add/remove the active-main-content class
+            document.getElementById("main-content").classList.toggle("active-main-content");
+            });
+        </script>
     </body>
 </html>
